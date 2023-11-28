19fc16f6afb18ceac3e71cbd5ec09a5595167b0b
Kirito Msao <55633676+abdelilahelhaddad@users.noreply.github.com>
2023-11-28 17:01:23 +0100

    :fire: :art: Commit Desktop Layout :sparkles: :globe_with_meridians:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        softOrange: "hsl(35, 77%, 62%)",
+        softRed: "hsl(5, 85%, 63%)",
+        offWhite: "hsl(36, 100%, 99%)",
+        grayishBlue: "hsl(233, 8%, 79%)",
+        darkBlue: "hsl(236, 13%, 42%)",
+        veryDarkBlue: "hsl(240, 100%, 5%)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        base: "0.9375rem",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 };
