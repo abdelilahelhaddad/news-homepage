a36a77ac14811571567dbe852bfd0a0ce0b77ee8
Kirito Msao <55633676+abdelilahelhaddad@users.noreply.github.com>
2023-11-30 17:39:14 +0100

    :iphone: :seedling: Mobile Responsive :sparkles: :iphone:

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import "./dist/style.css";
+
+const openMenu = document.querySelector("#openMenu");
+const closeMenu = document.querySelector("#closeMenu");
+const navbar = document.querySelector("#navbar");
+
+openMenu.addEventListener("click", () => {
+  openMenu.classList.replace("block", "hidden");
+  closeMenu.classList.replace("hidden", "block");
+  navbar.classList.replace("hidden", "flex");
+});
+
+closeMenu.addEventListener("click", () => {
+  openMenu.classList.replace("hidden", "block");
+  closeMenu.classList.replace("block", "hidden");
+  navbar.classList.replace("flex", "hidden");
+});
